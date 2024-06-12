9edf7ba0aa530b689aa40a34fa31e1141244dc39
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-06-12 16:00:28 +0530

    Add shipping screen as a private Route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import {CartScreen} from "./screens/CartScreen.tsx";
  import {LoginScreen} from "./screens/LoginScreen.tsx";
  import {RegisterScreen} from "./screens/RegisterScreen.tsx";
  import {ShippingScreen} from "./screens/ShippingScreen.tsx";
+ import {PrivateRoute} from "./Components/PrivateRoute.tsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -26,7 +27,10 @@ const router = createBrowserRouter(
             <Route  path={'/cart'} element={<CartScreen/>}/>
             <Route path={'/login'} element={<LoginScreen/>}/>
             <Route path={'/register'} element={<RegisterScreen/>}/>
-            <Route path={'/shipping'} element={<ShippingScreen/>}/>
+
+            <Route path={''} element={<PrivateRoute/>}>
+                <Route path={'/shipping'} element={<ShippingScreen/>}/>
+            </Route>
         </Route>
     )
 )
