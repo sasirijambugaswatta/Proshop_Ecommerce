ca997dcfc9bfd1ccbf17274248bd3fbbd470eb69
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-06-18 09:53:05 +0530

    create and getOrder

--- backend/controller/orderController.ts
+++ backend/controller/orderController.ts
@@ -6,21 +6,62 @@ import Order from "../models/order.model.js";
 //@route GET /api/orders/myorders
 //@access private
 const getMyOrders = asyncHandler(async (req:Request, res:Response) => {
-    res.send('get my orders');
+    const order = await Order.find({user: req.user?._id});
+    if (order){
+        res.status(200).json(order);
+    }else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 //@desc create new order
 //@route POST /api/orders
 //@access private
 const addOrderItems = asyncHandler(async (req:Request, res:Response) => {
-    res.send('add order items');
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice} = req.body;
+
+    if(orderItems && orderItems.length === 0){
+        res.status(400);
+        throw new Error('No order items');
+    }else {
+        const order = new Order({
+            orderItems : orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined
+            })),
+            user: req.user?._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice
+        });
+        const createdOrder = await order.save();
+        res.status(201).json(createdOrder);
+    }
 });
 
 //@desc get order by id
 //@route GET /api/orders/:id
 //@access private
 const getOrderById = asyncHandler(async (req:Request, res:Response) => {
-    res.send('get order by id');
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+    if (order){
+        res.status(200).json(order);
+    }else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 //@desc update order to paid
---
+++ frontend/src/slices/ordersApiSlice.ts
@@ -0,0 +1,22 @@
+import {apiSlice} from "./apiSlice.ts";
+import {ORDERS_URL} from "../../contants.ts";
+
+export const ordersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        createOrder: builder.mutation({
+            query: (order) => ({
+                url: ORDERS_URL,
+                method: 'POST',
+                body: {...order}
+            }),
+        }),
+        getOrders: builder.query({
+            query: () => ({
+                url: ORDERS_URL,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+    })
+});
+
+export const {useCreateOrderMutation, useGetOrdersQuery} = ordersApiSlice
